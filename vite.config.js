eda1fbcdef6babe1dd55d020bb7a779c40ce1ab2
Prakhar Tiwari <indianstudentexplolearner@gmail.com>
2023-05-19 08:31:56 +0530

    Make Basic Vue3JS App

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2015',
+    assetsInlineLimit: 0,
+    minify: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'your-bundle-name.js',
+        manualChunks: undefined,
+        chunkFileNames: undefined,
+        assetFileNames: undefined,
+      },
+    },
+  },
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
